716092912c8040c59b23b3af969935daf6157c90
Antonio Simei <aq.simei@gmail.com>
2021-08-17 23:31:03 -0300

    transactions modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import Modal from 'react-modal';
 import {GlobalStyle} from './styles/global'
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewtransactionModalOpen, setIsNewtransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewtransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewtransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal = {handleOpenNewTransactionModal}/>
       <Dashboard/>
+
+      <NewTransactionModal
+        isOpen = {isNewtransactionModalOpen}
+        onRequestClose = {handleCloseNewTransactionModal}
+      />
       <GlobalStyle/>
     </>
   );
