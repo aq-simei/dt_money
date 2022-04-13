6f4d1abe8d1ac534a62298e63067c3c92e524fc7
Antonio Simei <antonio.simei@codeminer42.com>
2022-04-13 15:14:03 -0300

    Fix test's describes

--- src/components/Summary/Summary.test.tsx
+++ src/components/Summary/Summary.test.tsx
@@ -1,30 +1,32 @@
-import {Summary} from '.';
-import {render, screen} from '../../utils/test-utils';
+import { Summary } from ".";
+import { render, screen } from "../../utils/test-utils";
 
 beforeEach(() => {
-    render(<Summary/>)
-}
-)
+  render(<Summary />);
+});
 
-describe ("Dashboard", () => {
-    describe("Renders the default Summary", () => {
-        it('Renders the Income Card', () => {
-            const incomeCardTitle = screen.getByText('Entradas');
-            const incomeCardAmount = screen.getByTestId('income-amount');
-            expect(incomeCardTitle).toBeInTheDocument();
-            expect(incomeCardAmount).toBeInTheDocument();
-        });
-        it ('Renders the Outcome Card', () => {
-            const outcomeCardTitle = screen.getByText('Saídas');
-            const outcomeCardAmount = screen.getByTestId('outcome-amount');
-            expect(outcomeCardTitle).toBeInTheDocument();
-            expect(outcomeCardAmount).toBeInTheDocument();
-        });
-        it('Renders the Total Card', () => {
-            const totalCardTitle = screen.getByText('Total');
-            const totalCardAmount = screen.getByTestId('total-amount');
-            expect(totalCardTitle).toBeInTheDocument();
-            expect(totalCardAmount).toBeInTheDocument();
-        });
-    })
-})        
+describe("Summary", () => {
+  describe("Renders the default cards", () => {
+
+    it("Renders the Income Card", () => {
+      const incomeCardTitle = screen.getByText("Entradas");
+      const incomeCardAmount = screen.getByTestId("income-amount");
+      expect(incomeCardTitle).toBeInTheDocument();
+      expect(incomeCardAmount).toBeInTheDocument();
+    });
+
+    it("Renders the Outcome Card", () => {
+      const outcomeCardTitle = screen.getByText("Saídas");
+      const outcomeCardAmount = screen.getByTestId("outcome-amount");
+      expect(outcomeCardTitle).toBeInTheDocument();
+      expect(outcomeCardAmount).toBeInTheDocument();
+    });
+
+    it("Renders the Total Card", () => {
+      const totalCardTitle = screen.getByText("Total");
+      const totalCardAmount = screen.getByTestId("total-amount");
+      expect(totalCardTitle).toBeInTheDocument();
+      expect(totalCardAmount).toBeInTheDocument();
+    });
+  });
+});
